612894dfa4a56407f523841c5804ba2b11b4ce3f
paige <paigetetz@gmail.com>
2023-04-08 14:05:22 -0400

    Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Storefront from './Components/Storefront';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<h1>MERN SHOE SHOP</h1>
+			<Storefront />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Components/Storefront.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Storefront(props) {
+  return (
+    <div className='storefront-wrapper'>
+      <h1>New Releases</h1>
+    </div>
+  );
+}
+
+export default Storefront;
