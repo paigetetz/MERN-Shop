9b76bfe9722dae959fadb602951f688ba77c3467
paige <paigetetz@gmail.com>
2023-04-08 14:24:00 -0400

    Inventory added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Storefront from './Components/Storefront';
 function App() {
 	return (
 		<div className='App'>
-			<h1>MERN SHOE SHOP</h1>
 			<Storefront />
 		</div>
 	);
--- src/Components/Storefront.jsx
+++ src/Components/Storefront.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
-
-function Storefront(props) {
+import React, {useState} from 'react';
+import inventoryData from '../Inventory';
+const Storefront = (props) => {
+const [inventory, setInventory] = useState(inventoryData);
+console.log(inventory);
   return (
     <div className='storefront-wrapper'>
       <h1>New Releases</h1>
+
     </div>
   );
 }
